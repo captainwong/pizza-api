493c9aa59e00d67aa657e5987973402b223134aa
i7-8700 <1281261856@qq.com>
2023-07-04 19:52:16 +0800

    updateOrder, deleteOrder

--- handlers/delete-order.js
+++ handlers/delete-order.js
@@ -1,8 +1,22 @@
-function deleteOrder(id) {
-    if (!id)
-        throw new Error('Order ID is required for deleting the order');
-    
-    return {};
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+function deleteOrder(orderId) {
+    return docClient.delete({
+        TableName: 'pizza-orders',
+        Key: {
+            orderId: orderId,
+        }
+    })
+        .promise()
+        .then(res => {
+            console.log('Order is deleted!', res);
+            return res;
+        })
+        .catch(e => {
+            console.log('Oops, order is not deleted :(', e);
+            throw e;
+        });
 }
 
 module.exports = deleteOrder;
--- handlers/update-order.js
+++ handlers/update-order.js
@@ -1,10 +1,31 @@
-function updateOrder(id, updates) {
-    if (!id || !updates)
-        throw new Error('Order ID and updates object are required for updating the order');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+function updateOrder(orderId, options) {
+    if (!options || !options.pizza || !options.address)
+        throw new Error('Both pizza and address are required for updating the order');
     
-    return {
-        message: `Order ${id} was successfully updated`
-    };
+    return docClient.update({
+        TableName: 'pizza-orders',
+        Key: {
+            orderId: orderId
+        },
+        UpdateExpression: 'set pizza = :p, address=:a',
+        ExpressionAttributeValues: {
+            ':p': options.pizza,
+            ':a': options.address
+        },
+        ReturnValues: 'ALL_NEW'
+    })
+        .promise()
+        .then(res => {
+            console.log('Order updated!', res);
+            return res.Attributes;
+        })
+        .catch(e => {
+            console.log('Oops, order is not updated :(', e);
+            throw e;
+        });
 }
 
 module.exports = updateOrder;
